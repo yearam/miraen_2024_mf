62a166aa1dc38f4bb73ffd886214c2d5c03043d0
neverout <neveroutz@gmail.com>
2024-07-11 14:58:58 +0900

    컨테이너 수정 사항 처리

---
+++ assets/js/ui2024.js
@@ -0,0 +1,25 @@
+function accordionInit(){
+    var $target = $('.contents-list-wrap .item-group-list'),
+        $btn = $target.find('button'),
+        $panel = $target.find('ul');
+
+    if(!$target.length){
+        return false;
+    }
+
+    $btn.on('click' , function(){
+        if($(this).hasClass('active')){
+            $(this).removeClass('active');
+            $(this).next('ul').slideUp(300);
+        } else {
+            $btn.removeClass('active')
+            $(this).addClass('active');
+            $panel.slideUp(300);
+            $(this).next('ul').slideDown(300);
+        }
+    })
+  }
+
+  $(function(){
+    accordionInit();
+  })
