944dea5444d66325c36659edb9b1b91f532ca8ef
neverout <neveroutz@gmail.com>
2024-07-18 15:21:05 +0900

    주제 레이아웃 - 학습 목차 추가

--- assets/js/ui2024.js
+++ assets/js/ui2024.js
@@ -20,10 +20,64 @@ function accordionInit() {
     })
 }
 
+function txtLineBr(){
+    var $target = $('.badge-lesson');
+    $target.each(function(){
+        var $len = $(this).text().length;
+        if($len > 2){
+            $(this).addClass('txt-br');
+        }
+    })
+}
 
+function dataGroupSlide(){
+	var $obj = $('.data.group-content'),
+		$btn = $obj.find('.inner-wrap a'),
+		$panel = $obj.find('.data-group-child-area');
+
+    if (!$obj.length) {
+        return false;
+    }
+
+    $obj.each(function(){
+        if($(this).hasClass('active')){
+            $(this).find('.data-group-child-area').slideDown(300);
+        }
+    });
+
+	$btn.on('click' , function(e){
+		e.preventDefault();
+		if($(this).parents('.data.group-content').hasClass('active')){
+			$(this).parents($obj).removeClass('active');
+			$(this).parents('.data.group-content').find('.data-group-child-area').slideUp(300);
+		} else {
+			$obj.removeClass('active');
+			$(this).parents($obj).addClass('active');
+			$panel.slideUp(300);
+			$(this).parents('.data.group-content').find('.data-group-child-area').slideDown(300);
+		}
+	});
+}
+
+function txtEllipsis(){
+	var $obj = $('body.lesson-2024 .period-contents .lesson-items .item .data');
+
+	if (!$obj.length) {
+        return false;
+    }
+
+	$obj.each(function(){
+		if($(this).find('.number-pages .badge').length === 0){
+			$(this).find('strong').addClass('ellipsis-multi')
+		}
+	})
+}
 
 $(function () {
     accordionInit();
     $('.swiper-button-prev,.swiper-button-next').show();
     $('.tooltip-trigger').webuiPopover();
-})
+    txtLineBr();
+    dataGroupSlide();
+    txtEllipsis();
+});
